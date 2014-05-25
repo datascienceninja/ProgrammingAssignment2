17264d2d323fd39066ca0f18232baacca8e74b17
datascienceninja <datascienceninja@gmail.com>
2014-05-25 10:12:54 -0700

    Completed Assignment
    Created cachematrix and test script.

---
+++ testscript.R
@@ -0,0 +1,15 @@
+matrix1<-matrix(1:4,2,2)   
+matrix1               
+inverse.matrix1<-solve(matrix1) 
+inverse.matrix1
+matrix1 * inverse.matrix1 
+matrix1 %*% inverse.matrix1
+
+m <- makeCacheMatrix() #initialize
+m                      # shows that m is now a list of functions
+class(m)               # shows that m is a list
+class(m$set)           # shows that the elements of the list are functions
+m$set(matrix(1:4,2,2)) #set the matrix
+m$get()                #get the matrix 
+cacheSolve(m)          #calculate the inverse 
+cacheSolve(m)          #when called again use the cached inverse  
